a9a465154e2c9fd8d9844c514719848909b47a66
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-12-11 21:59:26 +0200

    complain andexplore

--- pythonaholic/pages/login.js
+++ pythonaholic/pages/login.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import LoginForm from '../components/LoginForm'
 
 export default function complain() {
     return (
  <div>
      <LoginForm/>
+     <Footer/>
  </div>
     )
 }
