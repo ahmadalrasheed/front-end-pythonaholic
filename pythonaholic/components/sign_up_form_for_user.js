b569630df1138d6afea69935be4a64263f840cef
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-12-14 17:39:31 +0200

    sign up

--- pythonaholic/components/sign_up_form_for_user.js
+++ pythonaholic/components/sign_up_form_for_user.js
@@ -1,129 +1,107 @@
+import Head from "next/head";
+
 export default function SignUpFormForUsers(props) {
   return (
     <>
-      <div class="h-screen md:flex">
-        <div class="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
-          <div>
-           
-          <a href='/'><img src='logo.png '/></a>
+      <Head>
+        <link
+          href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          rel="stylesheet"
+          id="bootstrap-css"
+        />
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      </Head>
+      {/* <!------ Include the above in your HEAD tag ----------> */}
+      <form
+        onSubmit={props.event_handler}
+        class="bg-white   "
+        action="#"
+        method="POST"
+      >
+        <div class="login-reg-panel">
+          <div class="login-info-box">
+            <h2>Create Your Account To Connect</h2>
+            <p>Post Your Service</p>
+            <label id="label-register" for="log-reg-show">
+              Company
+            </label>
+            <input
+              type="radio"
+              name="active-log-panel"
+              id="log-reg-show"
+              checked="checked"
+            />
           </div>
-          <div class="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-          <div class="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-          <div class="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-          <div class="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-        </div>
-        
-        <div class="flex md:w-1/2 justify-center py-10 items-center bg-white">
-          <form
-            onSubmit={props.event_handler}
-            class="bg-white   "
-            action="#"
-            method="POST"
-          >
-            {/* <h1 class="text-gray-800 font-bold text-2xl mb-1">Connect</h1> */}
-            
-            <h1 class="text-xl font-normal text-gray-600 mb-7">Create Your Account To <span className='text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-purple-600'>Connect</span> :</h1>
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <input
-                class="pl-2 outline-none border-none"
-                type="text"
-                name=""
-                id=""
-                placeholder="Full name"
-              />
-            </div>
 
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                />
-              </svg>
-              <input
-                class="pl-2 outline-none border-none"
-                type="text"
-                name="email"
-                id=""
-                placeholder="Email Address"
-              />
+          <div class="register-info-box">
+            <h2>Create Your Account To Connect</h2>
+            <p>Make an Offer</p>
+            <label id="label-login" for="log-login-show">
+              User
+            </label>
+            <input type="radio" name="active-log-panel" id="log-login-show" />
+          </div>
+
+          <div class="white-panel">
+            <div>
+              <div class="login-show">
+                <h2>Company Sign Up</h2>
+                <input type="text" placeholder="Full Name" />
+                <input type="text" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <input type="number" placeholder="Mobile Number" />
+                <div className="flex items-center gap-3">
+                  <label className="">ID_Card</label>
+                  <input
+                    id="ID_Card"
+                    accept="image/*"
+                    name="ID_Card"
+                    type="file"
+                    autoComplete="ID_Card"
+                    required
+                    placeholder="ID Card"
+                  />
+                </div>
+                <div className="flex items-center gap-3">
+                  <label className="">Commerical_Certificate</label>
+                  <input
+                    accept="image/*"
+                    id="CommericalCertificate"
+                    name="CommericalCertificate"
+                    type="file"
+                    autocomplete="current-password"
+                    required
+                  />
+                </div>
+                
+                <input type="button" type='submit' value="Sign Up" />
+              </div>
+              {/* <label className="">Password</label> */}
+              {/* <div className="inline-flex align-item-baseline">
+            <input
+              class="pl-2 outline-none border-none"
+              type="password"
+              name="password"
+              id=""
+              placeholder="Password"
+            /></div> */}
+
+              {/* <a href="">Forgot password?</a> */}
             </div>
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                />
-              </svg>
-              <input
-                class="pl-2 outline-none border-none"
-                type="number"
-                name="phonenumber"
-                id=""
-                placeholder="Mobile Number"
-              />
+            <div class="register-show">
+              <h2>User Sign Up</h2>
+              {/* <input type="text" placeholder="Full Name" name=""/> */}
+              <input type="text" placeholder="Email" name="email"/>
+              <input type="number" placeholder="Mobile Number" name="phonenumber" />
+              <input type="password" placeholder="Password" name="password" />
+              
+              <input type="button" type='submit' value="Sign Up" />
             </div>
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <input
-                class="pl-2 outline-none border-none"
-                type="password"
-                name="password"
-                id=""
-                placeholder="Password"
-              />
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
-            >
-              Sign Up
-            </button>
-            <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">
-              {/* Forgot Password ? */}
-            </span>
-          </form>
+          </div>
         </div>
-      </div>
+        <script src="/login_form.js"></script>
+      </form>
     </>
   );
 }
--- pythonaholic/pages/_app.js
+++ pythonaholic/pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import '../styles/mainScreen.css'
 import '../styles/userprofile.css'
+import '../styles/sign_up_form_for_user.css'
+
 import '../styles/companyprofile.css';
 
 import 'bootstrap/dist/css/bootstrap.css'; 
